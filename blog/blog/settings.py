95fd2f9aea0ed2e687f5ba97e9b4daabe330726e
madriz03 <javiermadriz3@gmail.com>
2023-04-04 22:24:36 +0000

    Agregue API_URL variable de entorno
    Agrego cambios en settings.py y en index.html para incluir variable de entorno API_URL

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -21,9 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+
 # Api de tinyMCE
 
 API_URL = API_URL
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = SECRET_KEY
